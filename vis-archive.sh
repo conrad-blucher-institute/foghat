644090ca7dde0d9a99966440e7592bcf0775213f
Niall Durham <niall.durham@tamucc.edu>
2019-07-26 23:42:26 -0500

    Attempt to automate visilibity archive d/l steps

---
+++ vis-archive.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+# Download href visibility archive data from Matthew Pyle's temporary store he setup for Waylon
+# Looks like he posts two months of data (~8GB) at a time
+URL='ftp://ftp.emc.ncep.noaa.gov/mmb/WRFtesting/mpyle/vis_tmp/'
+
+if [[ -z "$FOGHAT_BASE" || -z "$FOGHAT_LOG_DIR" || -z "$FOGHAT_ARCHIVE_DIR" ]]
+then
+    echo "?FOGHAT Environment variables not defined, see etc/sample-environment.sh" 1>&2
+    exit
+fi
+
+ARCHIVE_BASE=$FOGHAT_ARCHIVE_DIR/mpyle-href-vis
+ARCHIVE_DIR=$ARCHIVE_BASE/latest
+TODAY=`date -u '+%Y%m%d'`
+LOG_FILE="$FOGHAT_LOG_DIR/vis_archive-$TODAY.log"
+
+mkdir -p $FOGHAT_LOG_DIR $ARCHIVE_DIR
+
+#/usr/bin/wget -nv --no-parent -r --limit-rate=5m --wait=5 --timestamping --append-output=$LOG_FILE -nd --directory-prefix=$ARCHIVE_DIR/$date $URL
+
+# Calculate md5sums of all archive files and rename directory as date range
+pushd
+cd $ARCHIVE_DIR
+first=`ls -1 href.* | cut -c 6-13 | head -1`
+last=` ls -1 href.* | cut -c 6-13 | tail -1`
+/usr/bin/md5sum href.*.tar > href-$first-$last.md5
+cd ..
+mv $ARCHIVE_DIR "$first-$last"
+popd
+
