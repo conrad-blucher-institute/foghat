32f4d2c9222eeda2cc9863788e764938899f6d59
Niall Durham <niall.durham@tamucc.edu>
2019-07-27 14:11:33 -0500

    Fix bugs in visualization archive d/l script

--- vis-archive.sh
+++ vis-archive.sh
@@ -10,17 +10,16 @@ then
     exit
 fi
 
-ARCHIVE_BASE=$FOGHAT_ARCHIVE_DIR/mpyle-href-vis
-ARCHIVE_DIR=$ARCHIVE_BASE/latest
+ARCHIVE_DIR=$FOGHAT_ARCHIVE_DIR/mpyle-href-vis/latest
 TODAY=`date -u '+%Y%m%d'`
 LOG_FILE="$FOGHAT_LOG_DIR/vis_archive-$TODAY.log"
 
 mkdir -p $FOGHAT_LOG_DIR $ARCHIVE_DIR
 
-#/usr/bin/wget -nv --no-parent -r --limit-rate=5m --wait=5 --timestamping --append-output=$LOG_FILE -nd --directory-prefix=$ARCHIVE_DIR/$date $URL
+/usr/bin/wget -nv --no-parent -r --limit-rate=5m --wait=5 --timestamping --append-output=$LOG_FILE -nd --directory-prefix=$ARCHIVE_DIR  $URL
 
 # Calculate md5sums of all archive files and rename directory as date range
-pushd
+pushd $PWD
 cd $ARCHIVE_DIR
 first=`ls -1 href.* | cut -c 6-13 | head -1`
 last=` ls -1 href.* | cut -c 6-13 | tail -1`
