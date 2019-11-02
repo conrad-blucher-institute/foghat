8eda507187c492fc8e9eb4e20e35a50cb9ab4c28
Niall Durham <niall.durham@tamucc.edu>
2019-11-02 03:04:56 -0500

    Environment stuff for IMAP access (NAM downloads)

--- etc/sample-environment.sh
+++ etc/sample-environment.sh
@@ -3,3 +3,8 @@ export FOGHAT_BASE=$HOME/fog-data
 export FOGHAT_LOG_DIR=$FOGHAT_BASE/logs
 export FOGHAT_ARCHIVE_DIR=$FOGHAT_BASE/archive
 export FOGHAT_COOKIES=$FOGHAT_BASE/.cookies
+
+# Only need these on server that's involved in NAM requests/downloads
+export FOGHAT_IMAP_HOST='imap.gmail.com'
+export FOGHAT_IMAP_USER='username'
+export FOGHAT_IMAP_PASSWD='password'
