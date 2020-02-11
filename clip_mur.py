ce6c2c1a31a77596773c246e53adbd4c6f636596
Niall Durham <niall.durham@tamucc.edu>
2020-02-10 19:59:46 -0600

    Added possibly useful output to MUR lat/lon check

--- clip_mur.py
+++ clip_mur.py
@@ -15,9 +15,9 @@ def clip(filename):
 
     # Code stolen/adapted from https://stackoverflow.com/a/29136166/1502174
 
-    # Hamid's MapS DLNN bounding box
-    latbounds = [ 27 , 28.5 ]
-    lonbounds = [ -97.7 , -96 ] # degrees east
+    # Hamid's [expanded] MapS DLNN bounding box
+    latbounds = [ 25.74 , 29.5 ]
+    lonbounds = [ -97.77 , -94 ] # degrees east
 
     lats = nc.variables['lat'][:]
     lons = nc.variables['lon'][:]
@@ -40,12 +40,15 @@ def clip(filename):
     print(f' • Latitude indexes are [{latli}, {latui}], longitude indexes are [{lonli},{lonui}]')
     print(f'ncks command invocations to crop this file:')
     print(f' • ncks --no_alphabetize -d lat,{latbounds_str} -d lon,{lonbounds_str} {filename} -O range_example.nc')
-    print(f' • ncks --no_alphabetize -d lat,{latli},{latui} -d lon,{lonli},{lonui} {filename} -O index_example.nc')
+    print(f' • ncks --no_alphabetize -d lat,{latli},{latui} -d lon,{lonli},{lonui} {filename} -O index_example.nc\n')
+
+    print(f'From the NetCDF file, latitude bounds {lats[latli]:.6f},{lats[latui]:.6f} ; longitude bounds {lons[lonli]:.6f},{lons[lonui]:.6f}')
+    print(f' • resulting grid dimensions {latui-latli} latitude, {lonui-lonli} longitude')
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('file', help='NetCDF file to modify in place')
+    parser.add_argument('file', help='NetCDF file to examine WRT latitude, longitude')
     args = parser.parse_args()
     if args.file:
         clip(args.file)
