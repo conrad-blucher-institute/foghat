43e998ed902d0e705d1ffab39d9e71f7e44bcd38
Niall Durham <niall.durham@tamucc.edu>
2020-05-13 03:36:29 -0500

    Allow sstat monitoring on HPC, name jobs, bugfix

--- queue-hpc-1year.sh
+++ queue-hpc-1year.sh
@@ -10,10 +10,11 @@ year=$1
 if [[ $year -lt 2009 || $year -gt 2030 ]]
 then
     echo "?Usage: $0 <year>"
+    exit
 fi
 
 echo "?Queueing jobs for data year $year"
-sbatch hpc_maps.sbatch -c 0  $year-01-01 $year-12-31
-sbatch hpc_maps.sbatch -c 6  $year-01-01 $year-12-31
-sbatch hpc_maps.sbatch -c 12 $year-01-01 $year-12-31
-sbatch hpc_maps.sbatch -c 18 $year-01-01 $year-12-31
+sbatch --job-name "fogm${year:2}00" hpc_maps.sbatch -c 0  $year-01-01 $year-12-31
+sbatch --job-name "fogm${year:2}06" hpc_maps.sbatch -c 6  $year-01-01 $year-12-31
+sbatch --job-name "fogm${year:2}12" hpc_maps.sbatch -c 12 $year-01-01 $year-12-31
+sbatch --job-name "fogm${year:2}18" hpc_maps.sbatch -c 18 $year-01-01 $year-12-31
