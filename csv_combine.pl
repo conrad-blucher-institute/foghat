0c2bbf3400c3a3605e68111e5e3b477defd0a0ca
Niall Durham <niall.durham@tamucc.edu>
2020-07-28 15:23:07 -0500

    Output data in CSV format

--- csv_combine.pl
+++ csv_combine.pl
@@ -5,13 +5,14 @@
 use strict;
 use warnings;
 use utf8;
+use 5.010;
 
 use Data::Dumper;
 
 # XXX  Depending on how many times this code is revisted, it might be
 #      worth turning cache and line into an object (w/ Moo).  However,
-#      this will impact portability and likely prevent running on the HPC
-#      at least not w/o extra pain, suffering and setup
+#      this *will* impact portability and likely prevent running on the
+#      HPC w/o extra software installation/configuration (pain, suffering)
 
 # Return empty cache
 sub cache_empty
@@ -51,13 +52,21 @@ sub cache_add
     $cache_href->{var_map}->{$name} = $line_href->{value};
 }
 
+# Output cache to stdout as single CSV line
 sub cache_emit
 {
-    my ($href) = @_;
-    warn Dumper($href);
-    # TODO  If parameter order doesn't yet exist, generate it and output header
-    # TODO  Adjust/format cache?  E.g., separate date, model cycle, forecast hour?
-    # TODO  Output cache as single CSV line
+    state @param_order;
+
+    my ($href) = @_;                    # cache
+    # If parameter order doesn't yet exist, generate it and output header
+    if (scalar(@param_order) == 0) {
+        # XXX  Numeric sort didn't produce my desired ordering (lexical, then numeric ordering), will have to customize further
+        #@param_order = sort { $a <=> $b } keys %{$href->{var_map}};
+        @param_order = sort keys %{$href->{var_map}};
+        print qq(model_cycle_time,prediction_time,lat,lon,") . join('","', @param_order) . qq("\n);
+    }
+    # XXX  Parse/modify cache values before printing?  E.g., separate date, model cycle, forecast hour
+    print join(',', @{$href}{qw(model_cycle pred_time lat lon)}, @{$href->{var_map}}{@param_order}) . "\n";
 }
 
 
