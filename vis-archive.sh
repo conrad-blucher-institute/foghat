aa129bd4eb8008135064c9ac3cbb0747e80b7eff
Niall Durham <niall.durham@tamucc.edu>
2020-08-20 16:27:10 -0500

    Send email to Niall when finished downloading

--- vis-archive.sh
+++ vis-archive.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-# Download href visibility archive data from Matthew Pyle's temporary store he setup for Waylon
+# Download href/sref visibility archive data from Matthew Pyle's temporary store he setup for Waylon
 #
 # Looks like he posts two months of data (~8GB) at a time.  Waylon
 # emails me whenever a new data set is made available
 
-PREFIX=${1:-href}
+PREFIX=${1:-sref}
 
 URL='ftp://ftp.emc.ncep.noaa.gov/mmb/WRFtesting/mpyle/vis_tmp/'
 
@@ -21,15 +21,31 @@ LOG_FILE="$FOGHAT_LOG_DIR/mpyle_archive-$TODAY.log"
 
 mkdir -p $FOGHAT_LOG_DIR $ARCHIVE_DIR
 
-/usr/bin/wget -nv --no-parent -r --limit-rate=5m --wait=5 --timestamping --append-output=$LOG_FILE -nd --directory-prefix=$ARCHIVE_DIR  $URL
+/usr/bin/wget -nv --no-parent -r --limit-rate=10m --wait=5 --timestamping --append-output=$LOG_FILE -nd --directory-prefix=$ARCHIVE_DIR  $URL
 
 # Calculate md5sums of all archive files and rename directory as date range
 pushd $PWD
 cd $ARCHIVE_DIR
-first=`ls -1 $PREFIX?* | cut -c 6-13 | head -1`
-last=` ls -1 $PREFIX?* | cut -c 6-13 | tail -1`
-/usr/bin/md5sum $PREXI?*.tar > $PREFIX-$first-$last.md5
+FIRST=`ls -1 $PREFIX?* | cut -c 6-13 | head -1`
+LAST=` ls -1 $PREFIX?* | cut -c 6-13 | tail -1`
+DATE_RANGE="$FIRST-$LAST"
+MD5_FILE="$PREFIX-$DATE_RANGE-md5.txt"
+/usr/bin/md5sum $PREFIX?*.tar > $MD5_FILE
 cd ..
-mv $ARCHIVE_DIR "$first-$last"
-popd
+mv $ARCHIVE_DIR "$DATE_RANGE"
 
+# Send email w/ download/checksum information
+mailx -s "$PREFIX download on $HOSTNAME" -a "$DATE_RANGE/$MD5_FILE"  $FOGHAT_NOTIFY_EMAIL <<EOL
+Attention meatbag,
+
+Download on $HOSTNAME, started on $TODAY, for $PREFIX data between $DATE_RANGE has finished.  MD5 checksum file attached.
+
+Logfile is located at $LOG_FILE
+
+HAND
+
+
+EOL
+
+# Go home
+popd
