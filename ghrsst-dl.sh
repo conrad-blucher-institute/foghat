555c76489a526420310d37dc087724f5c6ebbefc
Niall Durham <niall.durham@tamucc.edu>
2023-11-29 15:31:55 -0600

    Fix MUR download to work w/ NASA Earthdata system
    * Requires Earthdata account, links in code
    * Use arithmatic comparisons for date tests

--- ghrsst-dl.sh
+++ ghrsst-dl.sh
@@ -1,6 +1,19 @@
 #!/usr/bin/env bash
 
-if [[ -z "$FOGHAT_BASE" || -z "$FOGHAT_LOG_DIR" || -z "$FOGHAT_ARCHIVE_DIR" ]]
+# Group for High Resolution Sea Surface Temperature (GHRSST) Level 4 sea surface temperature analysis
+# version 4 Multiscale Ultrahigh Resolution (MUR) L4 analysis
+# AKA "MUR dataset"
+# FMI https://podaac.jpl.nasa.gov/dataset/MUR-JPL-L4-GLOB-v4.1
+
+# To download MUR data from NASA's Earthdata system, you need to:
+#
+# 1. Register an Earthdata login (if you don't have one already):
+#    https://urs.earthdata.nasa.gov/profile
+#
+# 2. Follow the instructions to setup your ~/.netrc file for wget:
+#    https://www.opendap.org/documentation/tutorials/ClientAuthentication.html#_wget
+
+if [[ -z "$FOGHAT_BASE" || -z "$FOGHAT_LOG_DIR" || -z "$FOGHAT_ARCHIVE_DIR" || -z "$FOGHAT_COOKIES" ]]
 then
     echo "?FOGHAT Environment variables not defined, see etc/sample-environment.sh" 1>&2
     exit
@@ -52,8 +65,11 @@ doy1=$((10#$doy1))
 doy2=$((10#$doy2))
 
 # Sanity checks
-[[ $year1 > $year2 ]] && echo "?ensure starting date ($year1$doy1) is before end date ($year2$doy2)" 1>&2 && usage
-[[ $year1 == $year2 && $doy1 > $doy2 ]] && echo  "?ensure starting date ($year1$doy1) is before end date ($year2$doy2)" 1>&2 && usage
+(( $year1 > $year2 )) && echo "?ensure starting date ($year1$doy1) is before end date ($year2$doy2)" 1>&2 && usage
+(( $year1 == $year2 && $doy1 > $doy2 )) && echo  "?ensure starting date ($year1$doy1) is before end date ($year2$doy2)" 1>&2 && usage
+
+# Make sure cookies file is private (b/c Earthdata session cookie)
+chmod 600 $FOGHAT_COOKIES
 
 now=`date`
 echo "?downloading GHRSST Level 4 files from $1 ($year1,$doy1) to $2 ($year2,$doy2) on $now"  >>$LOG_FILE
@@ -71,7 +87,9 @@ do
     ymd=`jul2ymd "$y" "$d"`
     printf -v day '%03d' $d
     # Use OPeNDAP server b/c regular HTTP/FTP _don't_ have a complete copy of the data archive (for some reason)
-    echo "https://podaac-opendap.jpl.nasa.gov/opendap/hyrax/allData/ghrsst/data/GDS2/L4/GLOB/JPL/MUR/v4.1/$y/$day/${ymd}090000-JPL-L4_GHRSST-SSTfnd-MUR-GLOB-v02.0-fv04.1.nc" >>$url_file
+    #echo "https://podaac-opendap.jpl.nasa.gov/opendap/hyrax/allData/ghrsst/data/GDS2/L4/GLOB/JPL/MUR/v4.1/$y/$day/${ymd}090000-JPL-L4_GHRSST-SSTfnd-MUR-GLOB-v02.0-fv04.1.nc" >>$url_file
+    # New NASA Earthdata d/l URL/system operational as of sometime in 2020?
+    echo "https://archive.podaac.earthdata.nasa.gov/podaac-ops-cumulus-protected/MUR-JPL-L4-GLOB-v4.1/${ymd}090000-JPL-L4_GHRSST-SSTfnd-MUR-GLOB-v02.0-fv04.1.nc" >>$url_file
     d=$((d + 1))
     # Handle day overflow.  366 b/c leap years, wget will fail on non-leap years
     if (( d > 366 ))
@@ -86,7 +104,9 @@ done
 #       I'll have to change up the loop/downloading to do that :\
 
 # Download the files
-/usr/bin/wget -nv --load-cookies $FOGHAT_COOKIES --save-cookies $FOGHAT_COOKIES $FOGHAT_WGET_OPTIONS --timestamping --append-output=$LOG_FILE --directory-prefix=$ARCHIVE_DIR --input-file=$url_file
+/usr/bin/wget -nv --load-cookies $FOGHAT_COOKIES --save-cookies $FOGHAT_COOKIES --keep-session-cookie $FOGHAT_WGET_OPTIONS --timestamping --append-output=$LOG_FILE --directory-prefix=$ARCHIVE_DIR --input-file=$url_file
+
+# TODO  Move downloaded MUR files into correct year directory under MUR archive?  Limit to basenames from $url_file in case of parallel runs?
 
 # Clean up temp file
 rm $url_file
